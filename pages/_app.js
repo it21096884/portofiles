165cba7e01867bbd24ba70c47be02175bda0d9e4
it21096884 <it21096884@my.sliit.lk>
2025-02-11 22:46:52 +0530

    Oshadi Senevirathna Portofile

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import {ThemeProvider } from "./context/ThemeContext";
-
+//oshadi senevirathna
 export default function App({ Component, pageProps }) {
   return (<ThemeProvider>
     <Component {...pageProps} />
